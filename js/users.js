94cdcfc301553c4223e4aaace8e77081be4237e8
karmelas <karmela.s@gmail.com>
2017-08-31 11:11:03 -0400

    Add files via upload

---
+++ js/users.js
@@ -0,0 +1,14 @@
+var users = [
+    {
+        name: 'Walter Beans',
+        phone: '555-555-1234',
+        title: 'manager',
+        office: '555-555-5555'
+    },
+    {
+        name: 'Jeff Greene',
+        phone: '555-555-2234',
+        title: 'developer',
+        office: '555-555-6666'
+    }
+];
